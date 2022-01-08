9a2cff21625572113fa4c077d54375591756b013
Gabriel Barbosa <gabriel.bc.dev@hotmail.com>
2022-01-08 14:46:39 -0300

    Add Project_IMC

---
+++ Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_IMC
+{
+    public class Pessoa
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome.ToLower(); }
+            set { nome = value.ToUpper(); }
+        }
+
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        private double CalcularIMC()
+        {
+            return Peso / Math.Pow(Altura, 2);
+        }
+        public double CalcularIMCPublico()
+        {
+            return CalcularIMC();
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Desafio_IMC
+{
+
+    class Program
+    {
+        static void ImprimeResultado(double imc)
+        {
+            if (imc < 18.5)
+            {
+                Console.WriteLine(string.Format("Abaixo do peso {0:0,0.00}", imc));
+            }
+            if (imc >= 18.5 && imc < 24.9)
+            {
+                Console.WriteLine(string.Format("Peso normal {0:0,0.00}", imc));
+            }
+            if (imc >= 25 && imc < 29.9)
+            {
+                Console.WriteLine(string.Format("Sobre peso {0:0,0.00}", imc));
+            }
+            if (imc >= 30 && imc < 34.9)
+            {
+                Console.WriteLine(string.Format("Obesidade Grau 1 {0:0,0.00}", imc));
+            }
+            if (imc >= 35 && imc < 39.9)
+            {
+                Console.WriteLine(string.Format("Obesidade Grau 2 {0:0,0.00}", imc));
+            }
+            else if (imc >= 40)
+            {
+                Console.WriteLine(string.Format("Obesidade Morbida {0:0,0.00}", imc));
+            }
+        }
+
+        static void ImprimirCabecalho(string texto)
+        {
+            Console.WriteLine("------------------------------ " + texto + " -------------------------\n");
+        }
+
+        static void ImprimirCabecalho(string texto, string subTitulo)
+        {
+            Console.WriteLine("------------------------------ " + texto + " -------------------------\n");
+            Console.WriteLine("------------------------------ " + subTitulo + " -------------------------\n");
+        }
+
+        static void ImprimirCabecalho(string texto, double numero)
+        {
+            Console.WriteLine("------------------------------ " + texto + " -------------------------\n");
+            Console.WriteLine("------------------------------ " + numero.ToString() + " -------------------------\n");
+        }
+
+        static double CalculaImc(double peso, double altura)
+        {
+            return peso / Math.Pow(altura, 2);
+        }
+
+        static string RetornaNome(string texto)
+        {
+            Console.Write(texto);
+            return Console.ReadLine();
+        }
+
+        static double RetornaPeso(string texto)
+        {
+            Console.WriteLine(texto);
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double RetornaAltura(string texto)
+        {
+            Console.WriteLine(texto);
+            return double.Parse(Console.ReadLine());
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+
+
+            ImprimirCabecalho("Calcular IMC", 0.0);
+
+            p.Nome = RetornaNome("Qual seu nome: ");
+
+            p.Peso = RetornaPeso("Qual seu Peso: ");
+
+            p.Altura = RetornaAltura("Qual sua altura? ");
+
+
+            //imc = CalculaImc(p.Peso, p.Altura);
+            ImprimeResultado(p.CalcularIMCPublico());
+
+            Console.WriteLine(p.Nome);
+
+
+        }
+    }
+}
